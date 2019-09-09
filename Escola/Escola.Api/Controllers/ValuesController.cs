0c3f9eb40b407a14efb3829b96a8a8ec550e82c4
Nelson <nelson.ash@outlook.com>
2019-09-09 18:56:29 -0300

    Controller Values

--- Escola/Escola.Api/Controllers/ValuesController.cs
+++ Escola/Escola.Api/Controllers/ValuesController.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Escola.Api.Controllers
 {
@@ -14,7 +11,7 @@ namespace Escola.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
